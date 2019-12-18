9106240bb5d9aded53fcb597fcac0ab9627d93cd
Luigi Scognamiglio <luigi.scognamiglio9@studenti.unina.it>
2019-12-18 17:37:10 +0100

    Terzo Commit

--- SecondaProva2/src/Test/TestGestorePrenotazioni.java
+++ SecondaProva2/src/Test/TestGestorePrenotazioni.java
@@ -28,8 +28,21 @@ public class TestGestorePrenotazioni {
 		Corso c2=new Corso("IS","Natella",9,02);
 		int num;
 		num=GestorePrenotazioni.Numero_prenotati( a1, c2);
+		GestorePrenotazioni.VisualizzaPrenotazioni(a1, c2);
 		assertEquals(0,num);
 		
+	}
+	@Test
+	public void test2VisualizzaPrenotazioniCorsoNull() {
+		Appello a1 =new Appello(LocalDate.of(2019, Month.AUGUST, 24),TipoData.ORALE,"SG1",01);
+		Corso c2=new Corso("IS","Natella",9,02);
+		Studente s=new Studente("Luigi","Scognamiglio","N46000000");
+		StudentePrenotato sp=new StudentePrenotato(s,a1,c2);
+		GestorePrenotazioni.PrenotaStudente(sp);
+		int num;
+		num=GestorePrenotazioni.Numero_prenotati(a1, null);
+		GestorePrenotazioni.VisualizzaPrenotazioni(a1, null);
+		assertEquals(0,num);
 	}
 
 
